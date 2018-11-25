ebf68fb74ea6b04255263e14169cd38a82b6dead
Thomas Klepzig <tklepzig@users.noreply.github.com>
2018-11-25 13:12:43 +0100

    Create selected-objects-to-ground.py

---
+++ selected-objects-to-ground.py
@@ -0,0 +1,7 @@
+import bpy
+context = bpy.context
+
+for obj in context.selected_objects:
+    mx = obj.matrix_world
+    minz = min((mx * v.co)[2] for v in obj.data.vertices)
+    mx.translation.z -= minz
